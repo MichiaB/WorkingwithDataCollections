b99b4ea631ebd229ff2e923a6625ebd0119d25c8
michia <michiagayepadilla@gmail.com>
2026-06-02 16:19:47 -0400

    "added to files"

--- partTwo.js
+++ partTwo.js
@@ -1,24 +1,97 @@
-// Part 1: Refactoring Old Code 
+ // Part 1: Refactoring Old Code 
 
-const data = [
+// csv data
 
-{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
- { id: "48", name: "Barry", occupation: "Runner", age: "25" },
- { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
- { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
- { id: "7", name: "Bilbo", occupation: "None", age: "111" }
+
+//const csv = [ "ID,Name,Occupation,Age \r\n\
+//\r\n 42,Bruce,Knight,41 \r\n\
+//\r\n 57,Bob,Fry Cook,19\r\n\
+//\r\n 63,Blaine,Quiz Master,58\r\n\
+//\r\n 98,Bill,Doctor’s Assistant,26" ];
+
+
+
+//let columns = 4; {
+
+//for(let row=0; row<csv.length;row++)
+  //  for(column=0;column<csv[row].length;column++)
+
+  //    if ( csv [row] [column] == "X") {
+   //   console.log (`The ${ csv [row] [column]} is at row ${row} and column ${column}`);
+   // }
+//}
+
+
+// Part 2: Expanding Functionality 
+
+//const csv = [ "ID,Name,Occupation,Age \r\n\
+//\r\n 42,Bruce,Knight,41 \r\n\
+//\r\n 57,Bob,Fry Cook,19\r\n\
+//\r\n 63,Blaine,Quiz Master,58\r\n\
+//\r\n 98,Bill,Doctor’s Assistant,26" ];
+
+
+
+
+const csv = "ID,Name,Occupation,Age";
+const columns = csv.split(",").length;
+
+console.log(columns); //4
+
+const csvData = [
+    {ID,Name,Occupation,Age},
+    {48,Barry,Runner,25}
+    {42, Bruce , Knight, 41},
+    {57, Bob ,Fry Cook, 19},
+    {63 , Blaine , Quiz Master , 58},
+    {98 , Bill , Doctor's,Assistant,26},
+    {7, Bilbo, None, 111}
 
 ]
 
 
-// Part 2: Expanding Functionality
+// Array
+const data = [
+    {id: "42",
+    name: "Bruce",
+    occupation: "Knight",
+    age: "41"},
 
-// Declare a variable that stores the number of columns in each row of data
-// within the CSV.
+    {id: "57",
+    name: "Bob",
+    occupation: "Fry Cook",
+    age:"19"
+    },
+
+    {id: "63",
+    name: "Blaine",
+    occupation: "Quiz Master",
+    age:"58",
+     },
+
+     {id: "98",
+    name: "Bill",
+    occupation: "Doctor's Assistant",     
+    age: "26",}
+];
 
 
-//Instead of hard-coding four columns per row, expand your code to accept 
-// any number of columns. 
-// This should be calculated dynamically based on the first row of data.
+// Part 4: Sorting and Manipulating Data
+
+let totalage = 0;
 
 
+for ( let i= 0; i < data.length; i++) {
+    totalage += Number (data[i].age);
+}
+
+
+// Part 5: Full Circle 
+
+let csv = "id,name,occupation,age\n";
+
+for ( let i=0; i < data.length; i++) {
+    csv += `${ data[i].id}. ${data[i].name}, ${data[i].occupation}, ${data[i].age}`
+}
+
+console.log(csv);
