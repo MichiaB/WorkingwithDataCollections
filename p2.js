f73b898f3810e35891f60c80c37c5b3b2278631b
michia <michiagayepadilla@gmail.com>
2026-06-08 13:49:49 -0400

    "fixed part one"

--- p2.js
+++ p2.js
@@ -4,49 +4,50 @@ let CSV =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 const str = CSV.split("\n");
 for (let index = 0; index < str.length; index++) {
-  console.log(str[index]);
+  let row = str[index];
+  const str1 = row.split(",");
+  console.log(str1);
 }
+
 
 //Declare a variable that stores the number of columns in each row of data within the CSV.
 //Instead of hard-coding four columns per row, expand your code to accept any number of columns.
 // This should be calculated dynamically based on the first row of data.
 
+console.log(str);
 const obj = [[0], [0]];
-for (let i = 0; i < obj.lenth; i++) console.log(obj);
+for (let i = 0; i < obj.length; i++) console.log(obj);
 
 //Store your results in a two-dimensional array.
 //Each row should be its own array, with individual entries for each column.
 //Each row should be stored in a parent array, with the heading row located at index 0.
 //Cache this two-dimensional array in a variable for later use.
 
+// let CSV2 = [];
 
+// for (let i = 0; i < CSV2.length; i++) {
+//   CSV2[0] = [];
+// }
 
+// console.log(CSV2);
 
-let CSV2 = new Array();
+// Part 3: Transforming Data
 
-for  (let i = 0 ; i < CSV2.length; i++){
-  CSV2[0] = new Array ();
-}
-
-console.log(CSV2);
-
-
-// Part 3: Transforming Data 
-
-
-//For each row of data in the result array produced by your code above, 
+//For each row of data in the result array produced by your code above,
 // create an object where the key of each value is the heading for that value’s column.
 //Convert these keys to all lowercase letters for consistency.
 //Store these objects in an array, in the order that they were originally listed.
 //Since the heading for each column will be stored in the object keys,
 //  you do not need to create an object for the heading row itself.
 
+// //const CsV3 = {
+//   //ID: "ID",
+//  // Name: "Name",
+//   //Occupation: "Occupation",
+//   //Age: "Age",
 
-const ID = {
-  a :42,
-  b: 57,
-  c: 63,
-  d: 98
-};
 
-console.log(ID.keys(ID));
+
+// };
+
+// console.log(Object.keys(CsV3));
