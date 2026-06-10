6d5eea58cf350f1318f1053f895063f212554504
michia <michiagayepadilla@gmail.com>
2026-06-10 13:50:51 -0400

    "update"

--- p2.js
+++ p2.js
@@ -105,8 +105,9 @@ console.log(people2);
 //   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 // ];
 
-let people3= people2.toReversed();
+let people3 = people2.reverse();
+people2.reverse();
+people2.reverse();
 
 console.log(people3);
 // console.log(typeof CSV);
-
